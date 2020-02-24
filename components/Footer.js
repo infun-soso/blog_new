3e9cccad5e1e16b25a9bcf6a2e776b0d44cd63f3
infun-soso <973394690@qq.com>
2020-02-24 11:10:31 +0800

    blog-009: [feature] add icp.

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,9 @@
 import '@/public/style/components/footer.css'
 const Footer = ()=>(
     <div className="footer-div">
-        {/* <div>系统由 React + Node + Ant Desgin驱动 </div>
-        <div>wyfs.top</div> */}
+        <div>Copyright © 2020 wyfs.top 版权所有ICP证：	
+          <a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">京ICP备18054136号-1</a> 
+        </div>
     </div>
 )
 
